674ff15ace3b7b8741f5d008c540b7869c292fd2
alyh <105599258+bigboi00@users.noreply.github.com>
2024-05-24 21:21:51 +0800

    In Progress

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,23 +1,23 @@
-//In Progress
-using System.ComponentModel.DataAnnotations;
-
+// Product class with properties for Id, Name, Price, Description, and ImageUrl
+// Annotating Name and Price properties with Required attribute
+// Annotating Name property with StringLength attribute
 namespace INFT3050.Models
 {
     public class Product
     {
-        public int Id { get; set; }
+        public int Id { get; set; } // Id property
 
-        [Required]
-        [StringLength(100)]
+        [Required] // Name property is required
+        [StringLength(100)] // Name property should have a maximum length of 100 characters
         public string Name { get; set; }
 
-        [Required]
+        [Required] // Price property is required
         public decimal Price { get; set; }
 
-        [StringLength(500)]
+        [StringLength(500)] // Description property should have a maximum length of 500 characters
         public string Description { get; set; }
 
-        [StringLength(100)]
+        [StringLength(100)] // ImageUrl property should have a maximum length of 100 characters
         public string ImageUrl { get; set; }
     }
 }
