71d268c57a5c3561fcf38d12acb949813c9b11c7
alyh <105599258+bigboi00@users.noreply.github.com>
2024-07-11 19:16:57 +0800

    In Progress (Week 8)

---
+++ ViewModel/RegisterVM.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace INFT3050.ViewModel
+{
+    public class RegisterVM
+    {
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+        [Compare("Password", ErrorMessage = "Password not matching")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        public string? ConfirmPassword { get; set; }
+    }
+}
